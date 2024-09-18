1e5f934fa94d7e21a3fd7efbc695deb66c3d509c
nilse <nilekl0117@skola.goteborg.se>
2024-09-18 09:49:37 +0200

    Fixed so if nothing is inputed the program don't crash

--- genrep.py
+++ genrep.py
@@ -9,10 +9,14 @@ resistor_value = resistor_value.split(' ')
 antal_resistorer = len(resistor_value)
 serieresistans = 0
 parallellresistans = 0
-for i in range(0,antal_resistorer):
-    serieresistans += int(resistor_value[i])
-    parallellresistans += (1 / int(resistor_value[i]))
-parallellresistans = 1 / parallellresistans
+try:
+    for i in range(0,antal_resistorer):
+        serieresistans += int(resistor_value[i])
+        parallellresistans += (1 / int(resistor_value[i]))
+    parallellresistans = 1 / parallellresistans
+except:
+    serieresistans = 0
+    parallellresistans = 0
 
 print(f'Serieresistans: {serieresistans} ohm')
 print(f'Parallellresistans: {parallellresistans} ohm')
